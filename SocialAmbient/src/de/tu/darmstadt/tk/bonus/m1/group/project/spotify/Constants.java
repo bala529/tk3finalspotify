7cb8a677da8412fe692cf14bc0a629ee8c350aeb
balu <balu.ls@live.com>
2015-07-17 03:20:21 +0200

    changes

---
+++ SocialAmbient/src/de/tu/darmstadt/tk/bonus/m1/group/project/spotify/Constants.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package de.tu.darmstadt.tk.bonus.m1.group.project.spotify;
+
+/**
+ * @author dinesh
+ *
+ */
+public class Constants {
+
+	public static final String REGEX = ":";
+		
+	public static final String artistFinder = "Homeradioartist";
+	public static final String trackFinder = "Homeradiotrack";
+	public static final String localPlayer = "Homeradiolocal";
+	
+	public static final String local = "/home/local";
+	public static final String spotify = "/tud/spotify";
+	public static final String broker = "tcp://test.mosquitto.org:1883";
+	public static final String clientId = "JavaSample";
+	public static final int qos = 2;
+	
+}
--- SocialAmbient/src/de/tu/darmstadt/tk/bonus/m1/group/project/spotify/SubscribeSpotify.java
+++ SocialAmbient/src/de/tu/darmstadt/tk/bonus/m1/group/project/spotify/SubscribeSpotify.java
@@ -9,7 +9,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import de.tu.darmstadt.tk.bonus.m1.group.project.facebook.Constants;
+import de.tu.darmstadt.tk.bonus.m1.group.project.spotify.*;
 
 /**
  * @author dinesh
